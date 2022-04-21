bf5a2d6d93a9c13bb098c2a7f25b70fdf9399c48
Ramesh Kumar <rkonit@outlook.com>
2022-04-21 21:18:26 +0530

    Add HitCounter construct to Stack
    Count requests issued to each URL path and store them in DynamoDB table

--- src/CdkWorkshop/CdkWorkshopStack.cs
+++ src/CdkWorkshop/CdkWorkshopStack.cs
@@ -1,13 +1,13 @@
 using Amazon.CDK;
-using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.APIGateway;
+using Amazon.CDK.AWS.Lambda;
 using Constructs;
 
 namespace CdkWorkshop
 {
     public class CdkWorkshopStack : Stack
     {
-        internal CdkWorkshopStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        public CdkWorkshopStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
             // Defines a new lambda resource
             var hello = new Function(this, "HelloHandler", new FunctionProps
@@ -17,10 +17,15 @@ namespace CdkWorkshop
                 Handler = "hello.handler" // file is "hello", function is "handler"
             });
 
+            var helloWithCounter = new HitCounter(this, "HelloHitCounter", new HitCounterProps
+            {
+                Downstream = hello
+            });
+
             // defines an API Gateway REST API resource backed by our "hello" function.
             new LambdaRestApi(this, "Endpoint", new LambdaRestApiProps
             {
-                Handler = hello
+                Handler = helloWithCounter.Handler
             });
         }
     }
