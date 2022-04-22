1637ed15c373f26fb951385bca99a307fd8d4bcf
Ramesh Kumar <rkonit@outlook.com>
2022-04-22 06:25:52 +0530

    Initialize CodePipeline

--- src/CdkWorkshop/PipelineStack.cs
+++ src/CdkWorkshop/PipelineStack.cs
@@ -1,5 +1,8 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.CodeCommit;
+using Amazon.CDK.AWS.CodePipeline;
+using Amazon.CDK.AWS.CodePipeline.Actions;
+using Amazon.CDK.Pipelines;
 using Constructs;
 
 namespace CdkWorkshop
@@ -13,7 +16,23 @@ namespace CdkWorkshop
             {
                 RepositoryName = "WorkshopRepo"
             });
-            // Pipeline code goes here
+
+            // The basic pipeline declaration. This sets the initial structure
+            // of our pipeline
+            var pipeline = new CodePipeline(this, "Pipeline", new CodePipelineProps
+            {
+                PipelineName = "WorkshopPipeline",
+
+                // Builds our source code outlined above into a could assembly artifact
+                Synth = new ShellStep("Synth", new ShellStepProps{
+                    Input = CodePipelineSource.CodeCommit(repo, "main"),  // Where to get source code to build
+                    Commands = new string[] {
+                        "npm install -g aws-cdk",
+                        "sudo apt-get install -y dotnet-sdk-3.1", // Language-specific install cmd
+                        "dotnet build"  // Language-specific build cmd
+                    }
+                }),
+            });
         }
     }
 }
